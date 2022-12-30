65238d839b59d1695c1ad109945db9a281ccc2c3
oyvind3 <oyvind.finsrud@hotmail.com>
2022-12-30 09:01:08 +0100

    changes

--- counter.js
+++ counter.js
@@ -1,23 +1,3 @@
-fetch("https://inportalediting.azurewebsites.net/api/visit/testpartitionkey/VISITOR")
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("CAN'T FETCH API REQUEST");
-    }
-  })
-  .then(data => {
-    data.forEach(user => {
-      const markup = `<li>${user.visitor}</li>`;
-
-      document.querySelector("visitor").insertAdjacentHTML('beforeend', markup);
-    })
-  })
-  .catch(error => console.log(error));
-//      console.log(data);
-//      displayVisitor(data)
-
-//function displayVisitor(data) {
-//  const visitor = data;
-//  const visitorDiv = document.getElementById("visitor");
-//}  
+fetch('https://inportalediting.azurewebsites.net/api/visit/testpartitionkey/VISITOR')
+      .then(response => response.json())
+      .then(json => console.log(json))
