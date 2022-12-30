88ff94cf62512601aefffd209a565357105d70f0
oyvind3 <oyvind.finsrud@hotmail.com>
2022-12-30 09:22:48 +0100

    rm yml files

--- counter.js
+++ counter.js
@@ -1,3 +1,8 @@
+const ul = document.getElementById('visitors');
+const list = document.createDocumentFragment();
 fetch('https://inportalediting.azurewebsites.net/api/visit/testpartitionkey/VISITOR')
       .then(response => response.json())
       .then(json => console.log(json))
+      .then((response) => {
+        return response.json();
+      })
